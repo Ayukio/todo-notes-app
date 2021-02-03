9fb45c7e488637c9bc55f081152ea4945a36e240
Yulia Shiryaeva <yusya022@mail.ru>
2021-02-03 23:40:45 +0300

    added main components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {About} from "./pages/About";
+import {Home} from "./pages/Home";
+import {Navbar} from "./components/Navbar"
 
 function App() {
     return (
         <BrowserRouter>
+            <Navbar/>
             <div className="container pt-4">
-                <h1>test</h1>
+                <Switch>
+                    <Route path={'/'} exact component={Home}/>
+                    <Route path={'/about'} component={About}/>
+                </Switch>
             </div>
         </BrowserRouter>
     );
---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export const About = () => {
+    return (
+        <div className="jumbotron">
+            <h1 className="display-4">Create your task easily!</h1>
+            <p className="lead">This is a simple app to help you</p>
+        </div>
+)
+}
