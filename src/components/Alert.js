67a3fdae6b973c91d82b9b2cc7a6d9bf774c38e6
Yulia Shiryaeva <yusya022@mail.ru>
2021-02-04 22:43:35 +0300

    created alerts

---
+++ src/components/Alert.js
@@ -0,0 +1,19 @@
+import React, {useContext} from "react"
+import {AlertContext} from "../context/alert/alert-context";
+
+export const Alert = () => {
+    const {alert, hide} = useContext(AlertContext)
+
+    if (!alert.visible) {
+        return null
+    }
+    return (
+        <div className={`alert alert-${alert.type || 'warning'} alert-dismissible`}>
+            <strong>Attention</strong>
+            {alert.text}
+            <button onClick={hide} className="close" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    )
+}
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,29 @@
-import React from "react"
+import React, {useContext, useState} from "react"
+import {AlertContext} from "../context/alert/alert-context";
 
 export const Form = () => {
+
+    const [value, setValue] = useState('')
+    const alert = useContext(AlertContext)
+
+    const submitHandler = e => {
+            e.preventDefault()
+
+            if (value.trim()) {
+                alert.show("Note was created!", 'success')
+            } else {
+                alert.show("Enter a title of note!")
+            }
+        }
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="form-group">
-                <input type="text" className="form-control" placeholder="Enter title of your note"/>
+                <input type="text"
+                       className="form-control"
+                       placeholder="Enter title of your note"
+                       value={value}
+                       onChange={e => setValue(e.target.value)}
+                />
             </div>
         </form>
     )
